741e387b91f62f4846ad8ae5e1dacbfbeba7f9c3
bulbul-rasel <rasel.bulbul7@gmail.com>
2021-03-05 23:10:51 +0600

    M6F2 & M6F3

--- Mid_Lab_Task/laratest/app/Http/Controllers/SystemController.php
+++ Mid_Lab_Task/laratest/app/Http/Controllers/SystemController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Controllers;
 use App\Physical_channel;
 use App\Ecommerce_channel;
@@ -7,25 +6,19 @@ use App\Social_media_channel;
 use App\Product;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
-
 use Illuminate\Http\Request;
-
 class SystemController extends Controller
 {
     public function index(){
         return view('system.sales');
     }
     public function physical_index(){
-
         $date_7 = \Carbon\Carbon::today()->subDays(7);
-
         $physical_products_7 = Physical_channel::where('sell_date','>=',$date_7)->get();
         $physical_counter_7 = count($physical_products_7);
-
         $date = \Carbon\Carbon::today();
         $physical_products = Physical_channel::where('sell_date','>=',$date)->get();
         $physical_counter = count($physical_products);
-
         return view('system.physical')
                     ->with('data',['physical_counter_7'=>$physical_counter_7,
                                             'physical_counter'=>$physical_counter]);
@@ -37,52 +30,43 @@ class SystemController extends Controller
         return view('system.ecommerce');
     }
     public function product_index(){
-
         $available_product = DB::table('products')->where('status', 'available')->get();
         $available_product_counter = count($available_product);
         
         $upcomming_product = DB::table('products')->where('status', 'upcomming')->get();
         $upcomming_product_counter = count($upcomming_product);
-
         return view('system.product')->with('data',['available_product_counter'=>$available_product_counter,
                                                         'upcomming_product_counter'=>$upcomming_product_counter]);
     }
-
     public function available_product_index(){
         
-        $productlist = Product::all();
+        $productlist = Product::paginate(10);
         
         return view('system.availableProduct')->with('list', $productlist);
     }
-
+    public function available_product_quantity_sort(){
+        
+        $productlist = Product::orderBy('id')->paginate(10);
+        
+        return view('system.availableProduct')->with('list', $productlist);
+    }
     public function sellCount(){
-
         //7day
-
         $date_7 = \Carbon\Carbon::today()->subDays(7);
-
         $physical_products_7 = Physical_channel::where('sell_date','>=',$date_7)->get();
         $physical_counter_7 = count($physical_products_7);
-
         $ecommerce_products_7 = Ecommerce_channel::where('sell_date','>=',$date_7)->get();
         $ecommerce_counter_7 = count($ecommerce_products_7);
-
         $social_products_7 = Social_media_channel::where('sell_date','>=',$date_7)->get();
         $social_counter_7 = count($social_products_7);
-
         //1day
-
         $date = \Carbon\Carbon::today();
         $physical_products = Physical_channel::where('sell_date','>=',$date)->get();
         $physical_counter = count($physical_products);
-
         $ecommerce_products = Ecommerce_channel::where('sell_date','>=',$date)->get();;
         $ecommerce_counter = count($ecommerce_products);
-
         $social_products = Social_media_channel::where('sell_date','>=',$date)->get();
         $social_counter = count($social_products);
-
-
         return view('system.sales')
                     ->with('data',['physical_counter_7'=>$physical_counter_7,
                                     'ecommerce_counter_7'=>$ecommerce_counter_7,
@@ -90,9 +74,7 @@ class SystemController extends Controller
                                             'physical_counter'=>$physical_counter,
                                                 'ecommerce_counter'=>$ecommerce_counter,
                                                     'social_counter'=>$social_counter]);
-
     }
-
     public function store(Request $req){
         $product = new Physical_channel();
         $product->product_id        = $req->product_id;
@@ -104,20 +86,42 @@ class SystemController extends Controller
         $product->unit_price        = $req->unit_price;
         $product->quantity          = $req->quantity;
         $product->total_price        = (($req->unit_price)*($req->quantity));
-
         $product->save();
-
         $req->session()->put('stored', "Data stored");
         
         return redirect()->route('system.physical_store');
     }
-
     public function product_edit($id){
-
         $product = Product::find($id);
-
         return view('product.edit')->with('product', $product);
 
+    }
+
+    public function product_update($id, Request $req){
+    public function product_update($id, ProductRequest $req){
+    
+            $validated = $req->validated();
+    
+            $product = Product::find($id);
+
+        $product->id                = $req->id;
+        $product->product_name      = $req->product_name;
+        $product->category          = $req->category;
+        $product->quantity          = $req->quantity;
+        $product->unit_price        = $req->unit_price;
+        $product->last_updated      = Carbon::now();
+        $product->status            = $req->status;
+        $product->save();
+        return redirect()->route('system.available_products');
+    }
+    public function product_delete($id){
+        $product = Product::find($id);
+        return view('product.delete')->with('product', $product);
+    }
+    
+    public function product_destroy($id){
+        $product = Product::destroy($id);
+        return redirect()->route('system.available_products');
     }
         
 }
--- Mid_Lab_Task/laratest/resources/views/home/edit.blade.php
+++ Mid_Lab_Task/laratest/resources/views/home/edit.blade.php
@@ -1,38 +1,37 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Create</title>
-</head>
-<body>
-
-<h1>User Id, {{$user['id']}}  </h1>
-
-<form method = "POST">
-	<fieldset>
-		<legend>Edit Users</legend>
-		<table>
-			<tr>
-				<td>Username</td>
-				<td><input type="text" name = "username" value = "Bulbul"></td>
-			</tr>
-			<tr>
-				<td>Password</td>
-				<td><input type="password" name = "password" value = "123"></td>
-			</tr>
-			<tr>
-				<td>Email</td>
-				<td><input type="text" name = "email" value = "bulbul@gmail.com"></td>
-			</tr>
-			
-			<tr>
-				<td><input type="Submit" name = "submit" value = "Update"></td>
-			</tr>
-		</table>
-	</fieldset>
-	</form>
-    
-</body>
-</html>
+   <fieldset>
+        <form method="post">
+            @csrf
+            <table>
+				<tr>
+					<td>id: </td>
+					<td><input type="text" name="id" value="{{ $product['id'] }}" readonly></td>
+				</tr>
+                <tr>
+					<td>Product Name: </td>
+					<td><input type="text" name="product_name" value="{{ $product['product_name'] }}" ></td>
+				</tr>
+                <tr>
+					<td>Category: </td>
+					<td><input type="text" name="category" value="{{ $product['category'] }}" ></td>
+				</tr>
+                <tr>
+					<td>Quantity: </td>
+					<td><input type="text" name="quantity" value="{{ $product['quantity'] }}" ></td>
+				</tr>
+                <tr>
+					<td>Unit Price: </td>
+					<td><input type="text" name="unit_price" value="{{ $product['unit_price'] }}" ></td>
+				</tr>
+                <tr>
+					<td>Status: </td>
+                    <td><input type="text" name="status" value="{{ $product['status'] }}" ></td>					    
+                    <td> 
+						<select name='status'>
+							<option value="available"> Available </option>
+							<option value="upcomming" > Upcomming </option>
+						</select>	
+					</td>					    
+				</tr>
+                <tr>
+                <tr>
+				</table>
